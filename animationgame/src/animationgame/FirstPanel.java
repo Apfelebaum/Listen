d8014bad260b7e346063fef3ecaf7d3096b02215
Axmann.Paul <Axmann.Paul@192.168.7.239>
2015-12-01 15:29:17 +0100

    a

--- animationgame/src/animationgame/FirstPanel.java
+++ animationgame/src/animationgame/FirstPanel.java
@@ -6,6 +6,10 @@ public class FirstPanel extends JPanel {
 	@Override public void paintComponent(java.awt.Graphics g){
 		g.fillRect(30, 50, 45, 30);
 		g.fillOval(60, 20, 40, 100);
+		
+		 int[] xPoints = {(int)pos.x, (int)pos.x+20, (int)pos.x+30, (int)pos.x+40, (int)pos.x+60, (int)pos.x+40, (int)pos.x+30, (int)pos.x+20};
+		    int[] yPoints = {(int)pos.y, (int)pos.y-10, (int)pos.y-30, (int)pos.y-10, (int)pos.y, (int)pos.y+10, (int)pos.y+30, (int)pos.y+10};
+		    int nPoints = xPoints.length;
 	}
 
 	public static void main(String[] args) {
@@ -16,3 +20,5 @@ public class FirstPanel extends JPanel {
 	}
 
 }
+
+
